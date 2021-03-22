00b7921ed5af9c479468d06e4e95534f1a68caaa
github username <github email address>
2021-03-22 19:59:35 -0400

    commit tree 74fcb042ab06e692cd48f83600d9d6e5f0a8bbb4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
-
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :authors, only: [:show, :index] do
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
 
   root 'posts#index'
 
